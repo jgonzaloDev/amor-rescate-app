e7505c3d692129fe9c3071d25d9ff05d161c5a3d
jgonzaloDev <quinonesramirezjuniorgonzalo@gmail.com>
2025-10-01 20:34:29 -0500

    Agrego federated-main.json

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "tenant_id" {
 
 variable "location" {
   type        = string
-  description = "Resource location"
+  description = "Resource location (e.g. eastus2)"
 }
 
 variable "resource_group_name" {
@@ -18,6 +18,9 @@ variable "resource_group_name" {
   description = "Resource group name"
 }
 
+# ===============================
+# Networking
+# ===============================
 variable "vnet_name" {
   type        = string
   description = "Virtual network name"
@@ -25,24 +28,27 @@ variable "vnet_name" {
 
 variable "address_space" {
   type        = list(string)
-  description = "Address space for the VNet"
+  description = "Address space for the VNet (e.g. [\"10.0.0.0/16\"])"
 }
 
 variable "subnet_ag_cidr" {
   type        = string
-  description = "Address range for Application Gateway subnet"
+  description = "Address range for Application Gateway subnet (e.g. 10.0.1.0/24)"
 }
 
 variable "subnet_integration_cidr" {
   type        = string
-  description = "Address range for Integration subnet"
+  description = "Address range for Integration subnet (e.g. 10.0.2.0/24)"
 }
 
 variable "subnet_privateendpoint_cidr" {
   type        = string
-  description = "Address range for Private Endpoint subnet"
+  description = "Address range for Private Endpoint subnet (e.g. 10.0.3.0/24)"
 }
 
+# ===============================
+# App Service
+# ===============================
 variable "app_service_plan_name" {
   type        = string
   description = "App Service Plan name"
@@ -53,6 +59,9 @@ variable "webapp_name" {
   description = "App Service (Windows Web App) name"
 }
 
+# ===============================
+# SQL Server
+# ===============================
 variable "sql_server_name" {
   type        = string
   description = "SQL Server name"
@@ -74,7 +83,25 @@ variable "sql_database_name" {
   description = "SQL Database name"
 }
 
+# ===============================
+# Key Vault
+# ===============================
 variable "key_vault_name" {
   type        = string
   description = "Azure Key Vault name"
+}
+
+# ===============================
+# Application Gateway
+# ===============================
+variable "appgateway_name" {
+  type        = string
+  description = "Application Gateway name"
+  default     = "appgateway-amorrescate"
+}
+
+variable "appgateway_publicip_name" {
+  type        = string
+  description = "Application Gateway Public IP name"
+  default     = "agw-public-ip"
 }
