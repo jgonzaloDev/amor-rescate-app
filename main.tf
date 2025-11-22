1bbef44480da79e3e7550fcd69535da03e4e7911
jgonzaloDev <quinonesramirezjuniorgonzalo@gmail.com>
2025-11-21 19:25:47 -0500

    1

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
-  required_version = ">= 1.7.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.34.0"
+      version = "~> 3.0"
     }
   }
+  required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
@@ -14,336 +14,66 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
 }
 
-# =========================================================
-# Resource Group
-# =========================================================
-resource "azurerm_resource_group" "rg" {
+# ============================================================
+# 1️⃣ GRUPO DE RECURSOS
+# ============================================================
+resource "azurerm_resource_group" "dojo" {
   name     = var.resource_group_name
   location = var.location
 }
 
-# =========================================================
-# Networking: VNet + Subnets
-# =========================================================
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = var.address_space
-}
-
-# Subnet Application Gateway
-resource "azurerm_subnet" "subnet_ag" {
-  name                 = "subnetAG"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = [var.subnet_ag_cidr]
-}
-
-# Subnet integración (delegada a App Service)
-resource "azurerm_subnet" "subnet_integration" {
-  name                 = "subnet-integracion"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = [var.subnet_integration_cidr]
-
-  delegation {
-    name = "webapp-delegation"
-    service_delegation {
-      name = "Microsoft.Web/serverFarms"
-      actions = [
-        "Microsoft.Network/virtualNetworks/subnets/action",
-      ]
-    }
-  }
-}
-
-# Subnet Private Endpoint
-resource "azurerm_subnet" "subnet_pe" {
-  name                          = "subnet-privateEndPoint"
-  resource_group_name           = azurerm_resource_group.rg.name
-  virtual_network_name          = azurerm_virtual_network.vnet.name
-  address_prefixes              = [var.subnet_privateendpoint_cidr]
-  private_endpoint_network_policies = "Disabled"
-}
-
-# =========================================================
-# App Service Plan (Windows) + Web App Windows
-# =========================================================
-resource "azurerm_service_plan" "plan" {
-  name                = var.app_service_plan_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Windows"
-  sku_name            = "B1"
-}
-
-resource "azurerm_windows_web_app" "web" {
-  name                = var.webapp_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_service_plan.plan.id
-
-  public_network_access_enabled = false
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  site_config {
-    always_on              = true
-    vnet_route_all_enabled = true
-  }
-
-  app_settings = {
-    WEBSITE_RUN_FROM_PACKAGE = "0"
-  }
-}
-
-# VNet Integration
-resource "azurerm_app_service_virtual_network_swift_connection" "web_integration" {
-  app_service_id = azurerm_windows_web_app.web.id
-  subnet_id      = azurerm_subnet.subnet_integration.id
-}
-
-# =========================================================
-# SQL Server + Database
-# =========================================================
-resource "azurerm_mssql_server" "sql" {
-  name                          = var.sql_server_name
-  resource_group_name           = azurerm_resource_group.rg.name
-  location                      = azurerm_resource_group.rg.location
-  version                       = "12.0"
-  administrator_login           = var.sql_admin_login
-  administrator_login_password  = var.sql_admin_password
-  public_network_access_enabled = false
-}
-
-resource "azurerm_mssql_database" "db" {
-  name           = var.sql_database_name
-  server_id      = azurerm_mssql_server.sql.id
-  sku_name       = "Basic"
-  zone_redundant = false
-}
-
-# =========================================================
-# Key Vault privado + RBAC
-# =========================================================
+# ============================================================
+# 2️⃣ KEY VAULT CON RBAC
+# ============================================================
 resource "azurerm_key_vault" "kv" {
   name                        = var.key_vault_name
-  location                    = azurerm_resource_group.rg.location
-  resource_group_name         = azurerm_resource_group.rg.name
+  resource_group_name         = azurerm_resource_group.dojo.name
+  location                    = azurerm_resource_group.dojo.location
   tenant_id                   = var.tenant_id
-  sku_name                    = "standard"
 
-  soft_delete_retention_days   = 7
-  purge_protection_enabled     = false
-  public_network_access_enabled = false
-  enable_rbac_authorization    = true
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = false
+
+  enable_rbac_authorization = true
 }
 
-resource "azurerm_role_assignment" "kv_secrets_officer" {
+# ============================================================
+# 3️⃣ ROL: Secrets Officer para GitHub (OIDC)
+# ============================================================
+resource "azurerm_role_assignment" "github_kv_secrets" {
   scope                = azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Secrets Officer"
-  principal_id         = azurerm_windows_web_app.web.identity[0].principal_id
+  principal_id         = var.github_principal_id
 }
 
-# =========================================================
-# Private DNS Zones
-# =========================================================
-resource "azurerm_private_dns_zone" "pdz_web" {
-  name                = "privatelink.azurewebsites.net"
-  resource_group_name = azurerm_resource_group.rg.name
+# ============================================================
+# 4️⃣ SECRETOS
+# ============================================================
+
+# BDdatos = bd_demo
+resource "azurerm_key_vault_secret" "bd_datos" {
+  name         = "BDdatos"
+  value        = var.secret_bd_datos
+  key_vault_id = azurerm_key_vault.kv.id
+
+  depends_on = [azurerm_role_assignment.github_kv_secrets]
 }
 
-resource "azurerm_private_dns_zone" "pdz_sql" {
-  name                = "privatelink.database.windows.net"
-  resource_group_name = azurerm_resource_group.rg.name
+# userbd = dojo
+resource "azurerm_key_vault_secret" "userbd" {
+  name         = "userbd"
+  value        = var.secret_userbd
+  key_vault_id = azurerm_key_vault.kv.id
+
+  depends_on = [azurerm_role_assignment.github_kv_secrets]
 }
 
-resource "azurerm_private_dns_zone" "pdz_kv" {
-  name                = "privatelink.vaultcore.azure.net"
-  resource_group_name = azurerm_resource_group.rg.name
-}
+# passwordbd = 123456789Da
+resource "azurerm_key_vault_secret" "passwordbd" {
+  name         = "passwordbd"
+  value        = var.secret_passwordbd
+  key_vault_id = azurerm_key_vault.kv.id
 
-resource "azurerm_private_dns_zone_virtual_network_link" "link_web" {
-  name                  = "link-webapp"
-  resource_group_name   = azurerm_resource_group.rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.pdz_web.name
-  virtual_network_id    = azurerm_virtual_network.vnet.id
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "link_sql" {
-  name                  = "link-sql"
-  resource_group_name   = azurerm_resource_group.rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.pdz_sql.name
-  virtual_network_id    = azurerm_virtual_network.vnet.id
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "link_kv" {
-  name                  = "link-kv"
-  resource_group_name   = azurerm_resource_group.rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.pdz_kv.name
-  virtual_network_id    = azurerm_virtual_network.vnet.id
-}
-
-# =========================================================
-# Private Endpoints
-# =========================================================
-resource "azurerm_private_endpoint" "pe_web" {
-  name                = "pe-webapp"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  subnet_id           = azurerm_subnet.subnet_pe.id
-
-  private_service_connection {
-    name                           = "webapp-privatelink"
-    private_connection_resource_id = azurerm_windows_web_app.web.id
-    is_manual_connection           = false
-    subresource_names              = ["sites"]
-  }
-
-  private_dns_zone_group {
-    name                 = "webapp-dns-group"
-    private_dns_zone_ids = [azurerm_private_dns_zone.pdz_web.id]
-  }
-}
-
-resource "azurerm_private_endpoint" "pe_sql" {
-  name                = "pe-sql"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  subnet_id           = azurerm_subnet.subnet_pe.id
-
-  private_service_connection {
-    name                           = "sql-privatelink"
-    private_connection_resource_id = azurerm_mssql_server.sql.id
-    is_manual_connection           = false
-    subresource_names              = ["sqlServer"]
-  }
-
-  private_dns_zone_group {
-    name                 = "sql-dns-group"
-    private_dns_zone_ids = [azurerm_private_dns_zone.pdz_sql.id]
-  }
-}
-
-resource "azurerm_private_endpoint" "pe_kv" {
-  name                = "pe-keyvault"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  subnet_id           = azurerm_subnet.subnet_pe.id
-
-  private_service_connection {
-    name                           = "kv-privatelink"
-    private_connection_resource_id = azurerm_key_vault.kv.id
-    is_manual_connection           = false
-    subresource_names              = ["vault"]
-  }
-
-  private_dns_zone_group {
-    name                 = "kv-dns-group"
-    private_dns_zone_ids = [azurerm_private_dns_zone.pdz_kv.id]
-  }
-}
-
-# =========================================================
-# Application Gateway
-# =========================================================
-resource "azurerm_public_ip" "agw_pip" {
-  name                = "agw-public-ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-data "azurerm_network_interface" "pe_web_nic" {
-  name                = azurerm_private_endpoint.pe_web.network_interface[0].name
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-locals {
-  webapp_pe_private_ip = data.azurerm_network_interface.pe_web_nic.ip_configuration[0].private_ip_address
-}
-
-resource "azurerm_application_gateway" "agw" {
-  name                = "appgateway-amorrescate"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  sku {
-    name = "Standard_v2"
-    tier = "Standard_v2"
-  }
-
-  autoscale_configuration {
-    min_capacity = 1
-    max_capacity = 2
-  }
-
-  gateway_ip_configuration {
-    name      = "gatewayipconfig"
-    subnet_id = azurerm_subnet.subnet_ag.id
-  }
-
-  frontend_port {
-    name = "port-80"
-    port = 80
-  }
-
-  frontend_ip_configuration {
-    name                 = "feip"
-    public_ip_address_id = azurerm_public_ip.agw_pip.id
-  }
-
-  backend_address_pool {
-    name         = "pool-webapp-pe"
-    ip_addresses = [local.webapp_pe_private_ip]
-  }
-
-  backend_http_settings {
-    name                  = "bhs-http"
-    cookie_based_affinity = "Disabled"
-    port                  = 80
-    protocol              = "Http"
-    request_timeout       = 30
-  }
-
-  http_listener {
-    name                           = "listener-http"
-    frontend_ip_configuration_name = "feip"
-    frontend_port_name             = "port-80"
-    protocol                       = "Http"
-  }
-
-  request_routing_rule {
-    name                       = "rule-webapp"
-    rule_type                  = "Basic"
-    http_listener_name         = "listener-http"
-    backend_address_pool_name  = "pool-webapp-pe"
-    backend_http_settings_name = "bhs-http"
-    priority                   = 10
-  }
-}
-
-# =========================================================
-# Outputs
-# =========================================================
-output "appgateway_public_ip" {
-  value       = azurerm_public_ip.agw_pip.ip_address
-  description = "IP pública del Application Gateway"
-}
-
-output "webapp_pe_private_ip" {
-  value       = local.webapp_pe_private_ip
-  description = "IP privado del Web App (Private Endpoint)"
-}
-
-output "sql_server_fqdn" {
-  value = azurerm_mssql_server.sql.fully_qualified_domain_name
-}
-
-output "key_vault_id" {
-  value = azurerm_key_vault.kv.id
+  depends_on = [azurerm_role_assignment.github_kv_secrets]
 }
--- variables.tf
+++ variables.tf
@@ -1,107 +1,37 @@
 variable "subscription_id" {
-  type        = string
-  description = "Azure subscription ID"
+  type = string
 }
 
 variable "tenant_id" {
-  type        = string
-  description = "Azure Entra tenant ID"
-}
-
-variable "location" {
-  type        = string
-  description = "Resource location (e.g. eastus2)"
+  type = string
 }
 
 variable "resource_group_name" {
-  type        = string
-  description = "Resource group name"
+  type = string
 }
 
-# ===============================
-# Networking
-# ===============================
-variable "vnet_name" {
-  type        = string
-  description = "Virtual network name"
+variable "location" {
+  type = string
 }
 
-variable "address_space" {
-  type        = list(string)
-  description = "Address space for the VNet (e.g. [\"10.0.0.0/16\"])"
-}
-
-variable "subnet_ag_cidr" {
-  type        = string
-  description = "Address range for Application Gateway subnet (e.g. 10.0.1.0/24)"
-}
-
-variable "subnet_integration_cidr" {
-  type        = string
-  description = "Address range for Integration subnet (e.g. 10.0.2.0/24)"
-}
-
-variable "subnet_privateendpoint_cidr" {
-  type        = string
-  description = "Address range for Private Endpoint subnet (e.g. 10.0.3.0/24)"
-}
-
-# ===============================
-# App Service
-# ===============================
-variable "app_service_plan_name" {
-  type        = string
-  description = "App Service Plan name"
-}
-
-variable "webapp_name" {
-  type        = string
-  description = "App Service (Windows Web App) name"
-}
-
-# ===============================
-# SQL Server
-# ===============================
-variable "sql_server_name" {
-  type        = string
-  description = "SQL Server name"
-}
-
-variable "sql_admin_login" {
-  type        = string
-  description = "SQL Server admin login"
-}
-
-variable "sql_admin_password" {
-  type        = string
-  description = "SQL Server admin password"
-  sensitive   = true
-}
-
-variable "sql_database_name" {
-  type        = string
-  description = "SQL Database name"
-}
-
-# ===============================
-# Key Vault
-# ===============================
 variable "key_vault_name" {
-  type        = string
-  description = "Azure Key Vault name"
+  type = string
 }
 
-# ===============================
-# Application Gateway
-# ===============================
-variable "appgateway_name" {
-  type        = string
-  description = "Application Gateway name"
-  default     = "appgateway-amorrescate"
+variable "github_principal_id" {
+  type = string
+  description = "Object ID del principal federado de GitHub"
 }
 
-variable "appgateway_publicip_name" {
-  type        = string
-  description = "Application Gateway Public IP name"
-  default     = "agw-public-ip"
+# Secretos
+variable "secret_bd_datos" {
+  type = string
+}
+
+variable "secret_userbd" {
+  type = string
+}
+
+variable "secret_passwordbd" {
+  type = string
 }
